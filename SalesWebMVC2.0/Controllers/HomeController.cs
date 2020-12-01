04a7b7cce43b7a35d8ee8973eba6d16cd160b2fc
Abner Stelmastchuk <stelmastchukabner@gmail.com>
2020-12-01 14:27:17 -0300

    First controller and Razor pages tests

--- SalesWebMVC2.0/Controllers/HomeController.cs
+++ SalesWebMVC2.0/Controllers/HomeController.cs
@@ -13,7 +13,8 @@ namespace SalesWebMVC2._0.Controllers {
         }
 
         public IActionResult About() {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Salles Web MVC App from C# Course.";
+            ViewData["Student"] = "Abner Stelmastchuk.";
 
             return View();
         }
